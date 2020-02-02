0eeb5dcdb820544d94e71bf59f1a0fe913d75d02
unknown <emadhanna@gmail.com>
2020-02-02 01:33:19 -0500

    First Commit

---
+++ src/main/java/com/stacksimplify/restservices/mac2020/SpringbootBuildingblocksMac2020Application.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.restservices.mac2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildingblocksMac2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildingblocksMac2020Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/stacksimplify/restservices/mac2020/SpringbootBuildingblocksMac2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.restservices.mac2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBuildingblocksMac2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
