c8d92a085dcecf9190cdd698c0182a3b1ff617d8
Emad Hanna <Emad_Hanna@cable.comcast.com>
2020-02-29 18:21:24 -0500

    Comminting the ApplicationTest.Java

--- src/test/java/com/stacksimplify/restservices/mac2020/SpringbootBuildingblocksMac2020ApplicationTests.java
+++ src/test/java/com/stacksimplify/restservices/mac2020/SpringbootBuildingblocksMac2020ApplicationTests.java
@@ -1,6 +1,6 @@
 package com.stacksimplify.restservices.mac2020;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -9,5 +9,4 @@ class SpringbootBuildingblocksMac2020ApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
